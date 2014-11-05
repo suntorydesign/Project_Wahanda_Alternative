f370a0cf2e9015235797540ef0e665cf66c6cf96
vietnt134 <vietnt134@gmail.com>
2014-11-05 08:08:12 +0700

    gift voucher send mail

--- Models/giftvoucher_model.php
+++ Models/giftvoucher_model.php
@@ -44,13 +44,13 @@ class giftvoucher_model extends Model {
 		// authentication enabled
 		$mail -> SMTPSecure = 'ssl';
 		// secure transfer enabled REQUIRED for GMail
-		$mail -> Host = "smtp.gmail.com";
+		$mail -> Host = SMTP_MAIL;
 		$mail -> Port = 465;
 		// or 587
 		$mail -> IsHTML(true);
-		$mail -> Username = "vietnt134@gmail.com";
-		$mail -> Password = "whathaveyoudone1341996";
-		$mail -> SetFrom("wahanda@gmail.com");
+		$mail -> Username = INFO_MAIL;
+		$mail -> Password = PASS_MAIL;
+		$mail -> SetFrom(INFO_MAIL, 'BELEZA VIETNAM');
 		$mail -> Subject = "Xác nhận chuyển gift voucher từ Beleza!";
 		$mail -> Body = $body;
 		$mail -> AddAddress($data['gift_voucher_email']);
