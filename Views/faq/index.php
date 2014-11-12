19bb9c89c78cbe7ce4a8499e0c05c9965a78504a
ltloibrights <ltloi@brights.vn>
2014-11-12 09:49:06 +0700

    fqa ui 100% done

--- Views/faq/index.php
+++ Views/faq/index.php
@@ -8,7 +8,7 @@
 
 <div id="content-wrap" >
 	<div id="faq-1">
-		<div class="container clear-fix">
+		<div class="container">
 			<div class="col-xs-1">
 				<img src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
 			</div>
@@ -26,48 +26,314 @@
 	<div id="faq-2" class="container">
 		<div class="faq-2-intro clearfix">
 			<div class="col-md-12">
-				<p>Chúng tôi đã có hàng ngàn salon... <a href="">Xem thêm>>></a></p>
+				<p>Chúng tôi đã có hàng ngàn salon và spa đánh giá của người tiêu dùng như bạn và lời khuyên chân thành từ rất kinh nghiệm hàng đầu trong blog của chúng tôi và Q & A phần. Nếu bạn đã có một trị liệu spa hoặc nuông chiều trong ngày, hoặc bạn là một người nhận may mắn của một trong những chứng từ spa của chúng tôi, thì tại sao không viết nhận xét của riêng bạn? Chúng tôi đang ở trên các cạnh của chỗ ngồi của chúng tôi chờ đợi để nghe nó như thế nào đi ... <a href="">Xem thêm>>></a></p>
 			</div>
 		</div>
 		<div class="faq-2-title-q clearfix">
-			<div class="col-md-12 ">
+			<div class="col-md-12">
 				ĐẶT CÂU HỎI CHO CHUYÊN GIA
 			</div>
 		</div>
-		<div class="col-md-5">
-			<form action="#" method="POST">
-				<textarea class="input-q" rows="10"></textarea>
-				<div class="clearfix">
-					<div class="col-md-6 remove-padding">
-						<i class="font-size:10px;">Bài viết không quá 100 từ</i>
+		<div class="faq-2-form clearfix">
+			<div class="col-md-5">
+				<form action="#" method="POST">
+					<textarea class="input-q" rows="10"></textarea>
+					<div class="clearfix">
+						<div class="col-md-6 remove-padding">
+							<i class="font-size:10px;">Bài viết không quá 100 từ</i>
+						</div>
+						<div class="col-md-6 remove-padding">
+							<button type="button" class="btn btn-block btn-orange">
+								<strong>GỬI</strong>
+							</button>
+						</div>
+						
 					</div>
-					<div class="col-md-6 remove-padding">
-						<button type="button" class="btn btn-block btn-orange">
-							<strong>GỬI</strong>
-						</button>
+				</form>
+
+			</div>
+			<div class="col-md-7">
+				<div class="clearfix">
+					<div class="faq-2-title-c">
+						CÁC CHỦ ĐỀ
+					</div>
+					<div class="clearfix">
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/cut-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>TÓC</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/face-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>FACE</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/eye-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>MẮT</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/hand-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>MÓNG</strong>
+							</div>
+						</div>
 					</div>
 					
-				</div>
-			</form>
-		</div>
-		<div class="col-md-7">
-			<div class="faq-2-title-c">
-				<div class="col-md-12 ">
-					CÁC CHỦ ĐỀ
+					<div class="clearfix">
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/moi-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>MÔI</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/yoga-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>YOGA</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/body-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>BODY</strong>
+							</div>
+						</div>
+						<div align="center" class="col-md-3">
+							<span class="fa-stack fa-lg fa-3x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-stack-1x fa-inverse">
+									<img src="<?php echo ASSETS . 'img/hair-icon.png' ?>" />
+								</i>
+							</span>
+							<div>
+								<strong>XĂM</strong>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
+		
+		<div class="divider"></div>
 	</div>
 
-	<div class="divider"></div>
+	
 
 	<div id="faq-3">
-		<div class="title">
+		<div class="container">
+			<div class="faq-3-title col-xs-12">
+				CÂU HỎI MỚI NHẤT
+			</div>
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi ?
+					</div>
+					<div class="source-q">
+						<i>Được ddawmg bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="answer-q">
+						Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời 
+						Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời  ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi Tiêu đề câu hỏi ?
+					</div>
+					<div class="source-q">
+						<i>Được ddawmg bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="answer-q">
+						Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời 
+						Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời Câu trả lời  ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-allq clearfix">
+				<button type="button" class="btn btn-default pull-right">
+					Xem tất cả
+				</button>
+			</div>
+
+			<div class="divider"></div>
 		</div>
-		<div class="cauhoi">
-		</div>	
+		
+		
+
+		<div class="container">
+			<div class="faq-3-title col-xs-12">
+				BÀI VIẾT VỀ SỨC KHỎE & SẮC ĐẸP MỚI NHẤT
+			</div>
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết
+					</div>
+					<div class="source-q">
+						<i>Được ddawmg bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="answer-q">
+						Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						 Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						  Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						   Nội dung bài viết ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết
+					</div>
+					<div class="source-q">
+						<i>Được ddawmg bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="answer-q">
+						Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						 Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						  Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						   Nội dung bài viết ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-allq clearfix">
+				<button type="button" class="btn btn-default pull-right">
+					Xem tất cả
+				</button>
+			</div>
+
+			<div class="divider"></div>
+		</div>
+
+		
+
+		<div class="container">
+			<div class="faq-3-title col-xs-12">
+				CÂU HỎI ĐƯỢC ĐÁNH GIÁ NỔI BẬT
+			</div>
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết
+					</div>
+					<div class="source-q">
+						<i>Được đánh giá bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="rating-q">
+						<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i>
+					</div>
+					<div class="answer-q">
+						Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						 Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						  Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						   Nội dung bài viết ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-q clearfix">
+				<div class="image col-xs-1">
+					<img class="image-responsive" src="<?php echo ASSETS . 'img/message-icon.png' ?>" />
+				</div>
+
+				<div class="col-xs-11">
+					<div class="title-q">
+						Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết Tiêu đề bài viết
+					</div>
+					<div class="source-q">
+						<i>Được đánh giá bởi Hoanghau34 </i>|<i> Cách đây 1 giờ</i>
+					</div>
+					<div class="rating-q">
+						<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i>
+					</div>
+					<div class="answer-q">
+						Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						 Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						  Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết Nội dung bài viết
+						   Nội dung bài viết ... <a href="#">Xem thêm>>></a>
+					</div>
+				</div>
+			</div>	
+
+			<div class="faq-3-allq clearfix">
+				<button type="button" class="btn btn-default pull-right">
+					Xem tất cả
+				</button>
+			</div>
+		</div>
+
 	</div>
 
-	<div class="divider"></div>
+	
 
 </div>
